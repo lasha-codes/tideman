81d1064912420abc683df5252e6ff329848508f1
lasha-codes <lashagiorgi420@gmail.com>
2024-03-12 13:02:56 -0700

    finished product

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
@@ -11,6 +11,98 @@ const App = () => {
   const [BobVotes, setBobVotes] = useState([])
   const [AliceVotes, setAliceVotes] = useState([])
   const [charlieVotes, setCharlieVotes] = useState([])
+  const [bobRanks, setBobRanks] = useState([])
+  const [aliceRanks, setAliceRanks] = useState([])
+  const [charlieRanks, setCharlieRanks] = useState([])
+
+  useEffect(() => {
+    const bobAsFirst = bobRanks.filter((ranks) => {
+      return ranks.firstRank
+    })
+    const bobAsSecond = bobRanks.filter((ranks) => {
+      return ranks.secondRank
+    })
+    const bobAsThird = bobRanks.filter((ranks) => {
+      return ranks.thirdRank
+    })
+    const charlieAsFirst = charlieRanks.filter((ranks) => {
+      return ranks.firstRank
+    })
+    const charlieAsSecond = charlieRanks.filter((ranks) => {
+      return ranks.secondRank
+    })
+    const charlieAsThird = charlieRanks.filter((ranks) => {
+      return ranks.thirdRank
+    })
+    const aliceAsFirst = aliceRanks.filter((ranks) => {
+      return ranks.firstRank
+    })
+    const aliceAsSecond = aliceRanks.filter((ranks) => {
+      return ranks.secondRank
+    })
+    const aliceAsThird = aliceRanks.filter((ranks) => {
+      return ranks.thirdRank
+    })
+
+    if (bobAsFirst.length > aliceAsFirst.length) {
+      console.log('bob > alice')
+    } else if (aliceAsFirst.length > bobAsFirst.length) {
+      console.log('alice > bob')
+    } else if (aliceAsFirst.length === bobAsFirst.length) {
+      if (aliceAsSecond.length > bobAsSecond.length) {
+        console.log('alice > bob')
+      } else if (bobAsSecond.length > aliceAsSecond.length) {
+        console.log('bob > alice')
+      } else if (bobAsSecond.length === aliceAsSecond.length) {
+        if (bobAsThird.length > aliceAsThird.length) {
+          console.log('bob > alice')
+        } else if (aliceAsThird.length > bobAsThird.length) {
+          console.log('alice > bob')
+        } else {
+          console.log('alice === bob')
+        }
+      }
+
+      if (charlieAsFirst.length > aliceAsFirst.length) {
+        console.log('charlie > alice')
+      } else if (aliceAsFirst.length > charlieAsFirst.length) {
+        console.log('alice > charlie')
+      } else if (aliceAsFirst.length === charlieAsFirst.length) {
+        if (aliceAsSecond.length > charlieAsSecond.length) {
+          console.log('alice > charlie')
+        } else if (charlieAsSecond.length > aliceAsSecond.length) {
+          console.log('charlie > alice')
+        } else if (charlieAsSecond.length === aliceAsSecond.length) {
+          if (charlieAsThird.length > aliceAsThird.length) {
+            console.log('charlie > alice')
+          } else if (charlieAsThird.length < aliceAsThird.length) {
+            console.log('alice > charlie')
+          } else {
+            console.log('alice === charlie')
+          }
+        }
+      }
+    }
+    if (bobAsFirst.length > charlieAsFirst.length) {
+      console.log('bob > charlie')
+    } else if (charlieAsFirst.length > bobAsFirst.length) {
+      console.log('charlie > bob')
+    } else if (charlieAsFirst.length === bobAsFirst.length) {
+      if (charlieAsSecond.length > bobAsSecond.length) {
+        console.log('charlie > bob')
+      } else if (bobAsSecond.length > charlieAsSecond.length) {
+        console.log('bob > charlie')
+      } else if (bobAsSecond.length === charlieAsSecond.length) {
+        if (bobAsThird.length > charlieAsThird.length) {
+          console.log('bob > charlie')
+        } else if (charlieAsThird.length > bobAsThird.length) {
+          console.log('charlie > bob')
+        } else {
+          console.log('charlie === bob')
+        }
+      }
+    }
+  }, [bobRanks, aliceRanks, charlieRanks])
 
   const addCandidate = (candidate, idx) => {
     setCheckDups((prev) => [...prev, candidate])
@@ -18,7 +110,7 @@ const App = () => {
     setSelectedCandidates((prev) => [...prev, candidate])
   }
 
-  const removeCandidate = (idxToRemove) => {
+  const removeCandidate = async (idxToRemove) => {
     const newSelectedCandidates = selectedCandidates.filter(
       (candidate, idx) => {
         return idxToRemove !== idx
@@ -34,7 +126,7 @@ const App = () => {
   const addVote = (e) => {
     e.preventDefault()
     if (selectedCandidates.length !== 3) {
-      return alert('All of the candidates must be ranked')
+      return
     }
     setVotesArr((prev) => [
       ...prev,
@@ -72,6 +164,36 @@ const App = () => {
         ])
       }
     })
+
+    BobVotes.forEach((vote) => {
+      if (vote.rank === 1) {
+        setBobRanks((prev) => [...prev, { firstRank: vote.rank }])
+      } else if (vote.rank === 2) {
+        setBobRanks((prev) => [...prev, { secondRank: vote.rank }])
+      } else {
+        setBobRanks((prev) => [...prev, { thirdRank: vote.rank }])
+      }
+    })
+
+    AliceVotes.forEach((vote) => {
+      if (vote.rank === 1) {
+        setAliceRanks((prev) => [...prev, { firstRank: vote.rank }])
+      } else if (vote.rank === 2) {
+        setAliceRanks((prev) => [...prev, { secondRank: vote.rank }])
+      } else {
+        setAliceRanks((prev) => [...prev, { thirdRank: vote.rank }])
+      }
+    })
+
+    charlieVotes.forEach((vote) => {
+      if (vote.rank === 1) {
+        setCharlieRanks((prev) => [...prev, { firstRank: vote.rank }])
+      } else if (vote.rank === 2) {
+        setCharlieRanks((prev) => [...prev, { secondRank: vote.rank }])
+      } else {
+        setCharlieRanks((prev) => [...prev, { thirdRank: vote.rank }])
+      }
+    })
   }
 
   return (
@@ -96,7 +218,7 @@ const App = () => {
         <div className='selected-candidates'>
           {selectedCandidates.map((item, idx) => {
             return (
-              <div key={idx}>
+              <div key={idx} className='shake'>
                 {idx + 1}.<span>{item}</span>
                 <span className='remove' onClick={() => removeCandidate(idx)}>
                   X
@@ -130,7 +252,10 @@ const App = () => {
         })}
       </div>
 
-      <button onClick={showWinner}>Show the winner</button>
+      <button onClick={showWinner} disabled={votesArr.length === 0}>
+        Show the winner
+        <span>(click 2 times)</span>
+      </button>
     </div>
   )
 }
